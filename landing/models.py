5c808b27a88ed0fcfab823c7c898a9e358233d13
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-06-10 13:56:26 +0300

    model Contact

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=100, verbose_name="ФИО")
+    phone_number = models.CharField(max_length=12, verbose_name="Номер телефона")
+
+    def __str__(self) -> str:
+        return self.full_name
