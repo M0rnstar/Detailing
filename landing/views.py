4c800364d313789af332a7c531f77062442eb86b
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-06-10 13:57:35 +0300

    sent email logic

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from .forms import ContactForm
 
 def home(request):
@@ -8,7 +9,17 @@ def contact_view(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            return redirect("success")
+            contact = form.save(commit=False)
+            name = contact.full_name
+            phone = contact.phone_number 
+            send_mail(
+                "Запись на детейлинг",
+                f"ФИО клиента: {name}\n Номер телефона клиента: {phone}",
+                "azamat.mambetov.2014@mail.ru",
+                ["azamat.mambetov.2014@gmail.com"]
+            )
+            contact.save()
+            return redirect("home")
     else:
         form = ContactForm()
     return render(request, 'contact.html', {'form': form})
